f93d3966aac4274602b3f23d5395f4cbd85d8ada
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-09 17:16:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'Meek Mill':'Dreams Worth More than Money'}
-function updatePlaylist{(playlist,artistName,songTitle)}{
+function updatePlaylist(playlist,artistName,songTitle){
   playlist.Drake = 'Bruh';
   return playlist
 }
